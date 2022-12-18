617c4ad89200cffb614a7d91060bed93e91e5974
Ghaith Hachem <ghaith.hachem@gmail.com>
2022-12-18 08:20:35 +0100

    Day 16 wip

--- day15/day15.go
+++ day15/day15.go
@@ -20,16 +20,17 @@ type Range struct {
 }
 
 func (r *Range) overlap(other *Range) bool {
-	
-	return r.start <= other.start || r.end >= other.start ||
-	other.start <= r.start || other.end >= r.start
-
+	return !(other.end < r.start || r.end < other.start)
 }
 func (r *Range) join(other *Range) (res *Range){
 	res = &Range {
 		r.start,
 		r.end,
 		r.exclusions,
+	}
+
+	if other == nil {
+		return
 	}
 
 	if other.start < r.start {
@@ -88,44 +89,22 @@ func ParseInput(input []string) []Sensor {
 	return res
 }
 
-func (grid *Grid) FillLocations(sensor Sensor, gridSize Coordinate, line int) {
-	//Add the sensor and beacon to the grid
-	distance := sensor.location.manhattan(sensor.beacon)
-
-	minX := utils.Max(gridSize.x, sensor.location.x - distance)
-	maxX := utils.Min(gridSize.y, sensor.location.x + distance)
-	// minY := utils.Max(gridSize.x, sensor.location.y - distance)
-	// maxY := utils.Min(gridSize.y, sensor.location.y + distance)
-
-	// fmt.Println(minX, maxX, minY, maxY)
-
-	for i := minX; i < maxX; i++ {
-		// for j := minY; j < maxY; j++ {
-			next := Coordinate{i, line}
-			if sensor.location.manhattan(next) <= distance {
-				(*grid)[next] = Nothing
-			}
-		// }
-	}
-
-	(*grid)[sensor.location] = SensorType
-	(*grid)[sensor.beacon] = BeaconType
-
-}
-
-func (sensor *Sensor)	getBeaconLocations(line,min,max int) (res *Range) {
+func (sensor *Sensor)	getBeaconLocations(line,min,max int) *Range {
 	exclusions :=  make(map[Coordinate]bool)
 	distance := sensor.location.manhattan(sensor.beacon)
 	distanceToY := int(math.Abs(float64(sensor.location.y - line)))
+	if distanceToY > distance {
+		return nil
+	}
 	startX := utils.Max(min, sensor.location.x - (distance - distanceToY))
 	endX := utils.Min(max, sensor.location.x + (distance - distanceToY))
-	if startX > endX {
-		mid := startX
-		startX = endX
-		endX = mid
-	}
+	// startX := sensor.location.x - (distance - distanceToY)
+	// endX := sensor.location.x + (distance - distanceToY)
 	if sensor.beacon.y == line {
 		(exclusions)[sensor.beacon] =  true
+	}
+	if sensor.location.y == line {
+		(exclusions)[sensor.location] =  true
 	}
 	return &Range {
 		startX,
@@ -147,10 +126,35 @@ func Day15() {
 
 }
 
-func remove(s []int, i int) []int {
-    s[i] = s[len(s)-1]
-    return s[:len(s)-1]
+func remove(s []*Range, index int) []*Range {
+	if index >= len(s) {
+		return s
+	}
+	ret := make([]*Range, 0)
+	ret = append(ret, s[:index]...)
+	return append(ret, s[index+1:]...)
 }
+
+func reduce(ranges *[]*Range)  bool {
+	idx := 0
+	for len(*ranges) > 1 {
+		r := (*ranges)[idx]
+		*ranges = remove(*ranges,idx)
+		for j,r2 := range *ranges {
+				if r.overlap(r2) {
+					*ranges = remove(*ranges,j)
+					*ranges = append(*ranges, r.join(r2))
+					return true
+				}
+		}
+		*ranges = append(*ranges, r)
+		if len(*ranges) == 2 {
+			return false
+		}
+	}
+	return false
+}
+
 
 func part1(input []string, line int) int {
 	sensors := ParseInput(input)
@@ -172,45 +176,27 @@ func part2(input []string, min, max int) int {
 	ranges := make([]*Range,0)
 	y := min
 	for i := min; i < max; i++ {
+		ranges = []*Range{}
 		for _, sensor := range sensors {
-			ranges = append(ranges,sensor.getBeaconLocations(i, min, max))
-		}
-		if i == 11 {
-			for _, r := range ranges {
-				fmt.Print(*r)
-			}
-			fmt.Println()
-		}
-		//reduce
-		for len(ranges) > 1 {
-			first := ranges[0]
-			second := ranges[1]
-			ranges = ranges[2:]
-			if first.overlap(second) {
-				newRange := first.join(second)
-				ranges = append(ranges, newRange)
-			} else {
-				ranges = append(ranges, first,second)
-				if len(ranges) == 2 {
-					break
-				}
+			r := sensor.getBeaconLocations(i, min, max)
+			if r != nil {
+				ranges = append(ranges,r)
 			}
 		}
+		for reduce(&ranges) { }
 		if len(ranges) > 1 {
 			y = i
 			break
 		}
 	}
-
 	x := 0
 	if len(ranges) > 1 {
-		if ranges[0].start < ranges[1].start {
-			x = ranges[0].end - ranges[1].start
+		if ranges[0].end < ranges[1].start {
+			x = ranges[0].end + 1
 		} else {
-			x = ranges[1].end - ranges[0].start
+			x = ranges[1].end + 1
 		}
 	}
-
 
 	return x * 4000000 + y
 }
